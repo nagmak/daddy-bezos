e5ba0be75c82a244158be1e736c5505563672b9d
nagmak <nagmakapoor@gmail.com>
2022-10-15 12:16:37 -0400

    setup chrome extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Flex, Spacer } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Flex color='white' width="600px" height="800px">
+          Wanna leave a tip?
+        <Button w='100px' bg='green.500'>
+          69%
+        </Button>
+        <Button bg='green.500' w='100px'>
+          420%
+        </Button>
+        <Button w='100px' bg='green.500'>
+          200%
+        </Button>
+      </Flex>
       </header>
     </div>
   );
